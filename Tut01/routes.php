5567278b74c332d33e0af8d36c32da741098d80e
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-07-28 22:58:22 +1200

    Update routes.php

--- Tut01/routes.php
+++ Tut01/routes.php
@@ -1,15 +1,5 @@
-<?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+<?php
 
 /*
 |--------------------------------------------------------------------------
@@ -26,11 +16,8 @@
 |
 */
 
-
-
 Route::get('/', "WelcomeController@index");
 Route::get('contact', "WelcomeController@Contact");
-
 
 /*
 |--------------------------------------------------------------------------
@@ -53,4 +40,13 @@ Route::get('contact', "WelcomeController@Contact");
      return "bar";
  });
  
- 
+/*
+|--------------------------------------------------------------------------
+| Route with wild card
+|--------------------------------------------------------------------------
+|
+| Route::get('contact/{id}', "WelcomeController@show");
+|
+*/
+
+Route::get('contact/{id}', "WelcomeController@show"); 
